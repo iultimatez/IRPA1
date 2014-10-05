8202cf8f4b4d01cc341ee45cc6b0426d5059d661
iultimatez <appledynasty@gmail.com>
2014-10-05 14:09:48 +0800

    Add .gitignore, main file slightly modified

--- IRPA1.py
+++ IRPA1.py
@@ -1,2 +1,14 @@
+import re
+import porter_algorithm
+p = porter_algorithm.PorterStemmer()
 f = open ('IRPA1text.txt', 'r')
-print f
+stop_words_file = open('stop_words.txt', 'r')
+stop_words = stop_words_file.read().split()
+result = re.sub("[^A-Za-z]"," ",f.read().lower()).split()
+stop = filter(lambda x: not(x in stop_words), result) 
+stemmed = map(lambda x: p.stem(x, 0, len(x)-1), stop)
+solution = open('result.txt', 'w')
+solution.write('\n'.join(stemmed))
+f.close()
+stop_words_file.close()
+solution.close()
